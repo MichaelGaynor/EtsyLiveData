dcd02ffa707648eaeb5d4ed179464818a0f8dcbc
Michael Gaynor <jmichaelgaynor@gmail.com>
2015-10-13 07:40:55 -0400

    Finalish

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,34 @@
 
 (function () {
 
-  console.log('It Works!');
+  var etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop';
+
+  $.ajax({
+    url: etsyURL,
+    dataType: 'jsonp',
+    method: 'get'
+  }).then(function (response) {
+    console.log(response);
+
+    //   // console.log('It Works!');
+    //   // console.log($);
+    //   // console.log(_);
+
+    // var theItems = etsy;
+
+    //TEMPLATE STRING
+    var itemTemplate = $('#info-template').text();
+
+    //MAKE TEMPLATE FUNCTION
+    var renderTemplate = _.template(itemTemplate);
+
+    _.each(response.results, function (i) {
+
+      var freshHTML = renderTemplate(i);
+
+      $('#TheScript').append(freshHTML);
+    });
+  });
+
+  // // console.dir(etsy)
 })();
